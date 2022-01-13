f96bd0f4fff684b651e793042e4e2215ff4599eb
sonardevplus@gmail.com <171015eb>
2022-01-13 08:07:04 -0300

    Projeto criado!

---
+++ backend/src/main/java/br/app/cineminhaamor/CineminhaAmorApplication.java
@@ -0,0 +1,13 @@
+package br.app.cineminhaamor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineminhaAmorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineminhaAmorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/app/cineminhaamor/CineminhaAmorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.app.cineminhaamor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineminhaAmorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
